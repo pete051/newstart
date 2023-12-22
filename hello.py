6c713dbbd4de9423ea05d467eb9e501900d7b237
pete051 <pete051@gmail.com>
2023-12-22 21:05:01 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 18:12:49 2019
+
+@author: peter
+
+
+def hello():
+    """Prints "hello" and returns none"""
+    print("Hello")
+    
+hello()
+%reset
---
+++ pass.py
@@ -0,0 +1,7 @@
+password = "madness"
+attempt = input("Enter Password")
+
+if attempt == password:
+    print("Welcome")
+else:
+    print("wromg")
